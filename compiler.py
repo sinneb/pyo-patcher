9252a4c49dc8ec5bb6ca5e7c16dae786aa8b96fb
Arthur Bennis <sinneb@gmail.com>
2017-04-05 12:57:23 +0200

    incremental update

--- compiler.py
+++ compiler.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
-import json, sys, os, time, subprocess
-prevPID = 0
+import json, sys, os, time, subprocess, itertools
 
 def getfiles(dirpath):
     a = [s for s in os.listdir(dirpath)
          if os.path.isfile(os.path.join(dirpath, s))]
     a.sort(key=lambda s: os.path.getmtime(os.path.join(dirpath, s)),reverse=True)
     return a
+
+def grouper(n, iterable, fillvalue=None):
+    args = [iter(iterable)] * n
+    return itertools.izip_longest(fillvalue=fillvalue, *args)
 
 def createPyoScript(jsonstring):
     # unique mixerkeys
@@ -24,15 +27,21 @@ def createPyoScript(jsonstring):
             node['id'] = "pyo" + node['id'].replace(".","")
             # remove . in wires
             for wire in node['wires']:
-                wire[0] = "pyo" + wire[0].replace(".","")
+                # check for multiple subwires
+                for it in range(0,len(wire)/3):
+                    wire[it*3] = "pyo" + wire[it*3].replace(".","")
 
     # pyo header
     fileout.write("#!/usr/bin/env python\n")
     fileout.write("from pyo import *\n")
     fileout.write("from time import sleep\n")
     fileout.write("\n")
-    fileout.write("s = Server().boot()\n")
-    fileout.write("s.start()\n")
+    fileout.write("s = Server()\n")
+    fileout.write("s.setMidiInputDevice(99)\n")
+    fileout.write("s.boot().start()\n\n")
+    
+    # general dummy pyobject for init
+    fileout.write("dummy = LFO(freq=0)\n\n")
     
     # init pyo objects
     for node in parsed_json:
@@ -63,24 +72,27 @@ def createPyoScript(jsonstring):
         if node['type']!='tab':
             # iterate through "wires" in nodes
             for wire in node['wires']:
-                #[u'pyob32a8810e77fe8', 1, u'ratio', u'b32a8810.e77fe8', 2, u'index', u'b32a8810.e77fe8', 0, u'carrier']
-                #items = len(wire) / 3
-                # find wire targetobject
-                targettype = ""
-                for targetnode in parsed_json:
-                    if targetnode['type']!='tab' and targetnode['id']==wire[0]:
-                        targettype = targetnode['type']
-                        #print targettype
-                # handle special nodes
-                if targettype == 'out':
-                    fileout.write("%s.addInput(%s,%s)\n" % (wire[0],mixerkey,node['id']))
-                    # left and right channel
-                    fileout.write("%s.setAmp(%s,0,1)\n" % (wire[0],mixerkey))
-                    fileout.write("%s.setAmp(%s,1,1)\n" % (wire[0],mixerkey))
-                    mixerkey += 1
-                else:
-                    #[u'pyo2abb2adff7f916', 0, u'freq']
-                    fileout.write("%s.%s = %s\n" % (wire[0], wire[2], node['id']))
+                # wire contains all incoming wires per node like [u'pyob32a8810e77fe8', 1, u'ratio', u'b32a8810.e77fe8', 2, u'index', u'b32a8810.e77fe8', 0, u'carrier']
+                # split and handle per wire
+                subwirelist = list(grouper(3,wire))
+                
+                for subwire in subwirelist:
+                    # find subwire targetobject type
+                    targettype = ""
+                    for targetnode in parsed_json:
+                        if targetnode['type']!='tab' and targetnode['id']==subwire[0]:
+                            targettype = targetnode['type']
+
+                    # handle special nodes
+                    if targettype == 'out':
+                        fileout.write("%s.addInput(%s,%s)\n" % (subwire[0],mixerkey,node['id']))
+                        # left and right channel
+                        fileout.write("%s.setAmp(%s,0,1)\n" % (subwire[0],mixerkey))
+                        fileout.write("%s.setAmp(%s,1,1)\n" % (subwire[0],mixerkey))
+                        mixerkey += 1
+                    else:
+                        #[u'pyo2abb2adff7f916', 0, u'freq']
+                        fileout.write("%s.%s = %s\n" % (subwire[0], subwire[2], node['id']))
 
     fileout.write("\n")
     fileout.write("while True:\n")
@@ -88,28 +100,27 @@ def createPyoScript(jsonstring):
     fileout.close()
 
 # get "last modified" mtime from last modified file in "livepatches"
-#lasttime_prev = os.stat("./livepatches/" + getfiles("livepatches")[0]).st_mtime
-lasttime_prev = 0
-#while True:
-lasttime = os.stat("./livepatches/" + getfiles("livepatches")[0]).st_mtime
-if(lasttime!=lasttime_prev):
-    print "new file available"
-    # read last edited file in livepatches subdir
-    thejson = open("./livepatches/" + getfiles("livepatches")[0],"r")
-    jsonstring = thejson.read()
-    createPyoScript(jsonstring)
-    # kill and run pyo script
-    try:
-      proc
-    except NameError:
-      pass
-    else:
-      proc.kill()
-    proc = subprocess.Popen("exec ./pyoscript.py",shell=True)
-    #prevPID = proc.pid
-    #print "PID:", proc.pid
+lasttime_prev = os.stat("./livepatches/" + getfiles("livepatches")[0]).st_mtime
+
+while True:
+    lasttime = os.stat("./livepatches/" + getfiles("livepatches")[0]).st_mtime
+    if(lasttime!=lasttime_prev):
+        print "new file available"
+        # read last edited file in livepatches subdir
+        thejson = open("./livepatches/" + getfiles("livepatches")[0],"r")
+        jsonstring = thejson.read()
+        createPyoScript(jsonstring)
+        # kill and run pyo script
+        try:
+          proc
+        except NameError:
+          pass
+        else:
+          proc.kill()
+        proc = subprocess.Popen("exec ./pyoscript.py",shell=True)
     
-    lasttime_prev = lasttime
-time.sleep(1)
+        lasttime_prev = lasttime
+    
+    time.sleep(1)
 
 
--- pyoscript.py
+++ pyoscript.py
@@ -2,16 +2,26 @@
 from pyo import *
 from time import sleep
 
-s = Server().boot()
-s.start()
-pyo9805f5cd82aa08 = Mixer(outs=2).out()
-pyob32a8810e77fe8 = FM(mul = 1, index = 5, ratio = 0.5, carrier = 200, add = 0)
-pyoe2ea1254cac28 = Blit(mul = 100, harms = 40, freq = 100, add = 200)
+s = Server()
+s.setMidiInputDevice(99)
+s.boot().start()
 
-pyo9805f5cd82aa08.addInput(10,pyob32a8810e77fe8)
-pyo9805f5cd82aa08.setAmp(10,0,1)
-pyo9805f5cd82aa08.setAmp(10,1,1)
-pyob32a8810e77fe8.carrier = pyoe2ea1254cac28
+dummy = LFO(freq=0)
+
+pyo32f21fda1926f = Mixer(outs=2).out()
+pyob37bced3d5fca = Freeverb(size = 0.7, mul = 1, damp = 1, bal = 0.5, input = dummy, add = 0)
+pyo49427769567128 = FM(mul = 1, index = 5, ratio = 0.5, carrier = 100, add = 0)
+pyo3d7d708f1daaf = ChenLee(mul = 1, chaos = 0.1, add = 0, pitch = 0.1)
+pyod4cc8724128018 = Midictl(mul = 1, channel = 0, minscale = 0, maxscale = 50, init = 10, add = 0, ctlnumber = 100)
+pyoc7acdc6df4752 = Midictl(mul = 1, channel = 0, minscale = 20, maxscale = 1000, init = 100, add = 0, ctlnumber = 101)
+
+pyo32f21fda1926f.addInput(10,pyob37bced3d5fca)
+pyo32f21fda1926f.setAmp(10,0,1)
+pyo32f21fda1926f.setAmp(10,1,1)
+pyob37bced3d5fca.input = pyo49427769567128
+pyo49427769567128.ratio = pyo3d7d708f1daaf
+pyo49427769567128.index = pyod4cc8724128018
+pyo49427769567128.carrier = pyoc7acdc6df4752
 
 while True:
 	sleep(1)
